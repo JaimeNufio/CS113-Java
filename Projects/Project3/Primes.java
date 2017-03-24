1ca8f778ded16f2dfa878ec316c9c1d2aa9f4426
Jaime Nufio <jaimeenufio@gmail.com>
2017-03-24 11:41:22 -0400

    FINALLY GOT THE PRIME FINDER TO WORK

--- Projects/Project3/Primes.java
+++ Projects/Project3/Primes.java
@@ -1,8 +1,10 @@
 public class Primes{
 	
+	public static int primesCnt = 0;
+
 	public static void main(String[] schwifty){
 
-		int[] list = new int[100];
+		int[] list = new int[1000000];
 		for(int i = 0; i<list.length; i++){list[i]=i;}
 
 		sieve(list);
@@ -12,33 +14,44 @@ public class Primes{
 
 	public static void sieve(int[] list){
 
-		int nextInt=2, primeCount = 0, lastPrimeCount = 0;
-		int[] newList = list; //either I'm an idiot, or I can't edit the old list.
-		newList[1]=0; //1 is not Prime
-
-		while(primeCount != lastPrimeCount){
-			for (int i = nextInt*2; i<newList.length; i+=nextInt){//PrimeSearch
-				newList[i] = 0;	
+		int  start = 0, cap = (int) Math.ceil(Math.sqrt(list.length)); //largest number we care to check for
+		int[] newList = list;
+		newList[1] = 0; //one is not prime, lets avoid that problem
+ 
+		for (int i = 0; i<cap; i++){
+			start = findNextPrime(newList);
+			for (int j = start*2; j<list.length;j+=start){
+			//	if (newList[j] != 0)					 //Debug Purposes
+			//		System.out.println(newList[j]+" is not prime");
+				newList[j] = 0;
 			}
-			lastPrimeCount = primeCount;
-			primeCount = primeCheck(newList);
 		}
 		
-		for (int i = 0; i<newList.length; i++){
-			if (i !=0){
-				System.out.println(i);
+		primesCnt = 0; //Repurpose the variable used as a stepper as a final count of primes.
+		for (int num: newList){
+			if (num != 0)
+				primesCnt++;
+		}
+		System.out.print("Number of Primes from 0 to "+list.length+": "+primesCnt);
+		
+	}
+
+	public static int findNextPrime(int[] list){
+		int nextPrime = -1, cnt = 0;
+		for (int num:list){
+			if (num !=0){
+				cnt++;
+				if (cnt >= primesCnt){
+					nextPrime = num;
+					primesCnt++;
+					//System.out.println("Next int sweep: "+num); //Debug Purposes
+					//So this sweeps 2 twice, maybe b/c I skip 1?	
+					//quite honestly, I don't care, b/c it works.
+					break;
+				}
 			}
 		}
-
+		return nextPrime;
 	}
 
-	public static int primeCheck(int list){ //well, most of the numbers counted wont be prime, but they will be possible primes
-		int cnt=0;
-		for(int i = 0; i<list.length; i++){
-			if (i != 0){
-				cnt++;
-			}
-		}	
-		return cnt;
-	}
 }
