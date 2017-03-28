ebff23d158e5bd6cae6783f5b36565bbbc42448c
Jaime Nufio <jaimeenufio@gmail.com>
2017-03-28 18:17:47 -0400

    Optimizing Algorithims

--- Projects/Project3/Primes.java
+++ Projects/Project3/Primes.java
@@ -9,7 +9,7 @@ public class Primes{												//tab width 4
 		for(int i = 0;i<list.length;i++){
 			list[i] = i+1;											 //initialize as 1 to X, not 0 to  X-1
 		}
-		sieve(list);
+		//sieve(list);
 		goldbach(list);	
 	}
 
@@ -17,29 +17,30 @@ public class Primes{												//tab width 4
 	
 		String primeLS = "Primes from 1 to "+list.length+":";		
 
-		int[] newList = list;		
 		int start = 0, scanNum, lastScanNum;		
 		
-		primeCNT = 0; 												//reset global variable 
+		primeCNT = 1; 												//reset global variable 
 		list[0] = 0;										 		//1 is not a prime number
 																	//and if it were, counting it would set everything to 0
-		scanNum = findNextZero(newList);	
+		scanNum = findNextNonZero(list);	
 		lastScanNum = -1;
 		
 		while(scanNum != -1){										//the findNextZero method throws -1 when no new primes are found
 			
 			for (int j = (scanNum*2)-1; j<list.length;j+=scanNum){	//start zeroing from the second multiple of the num for this loop 
 				
-					if( newList[j] != 0){							//the -1 is b/c of how we shifted the loop in init
-						newList[j] = 0;	
+					if( list[j] != 0){								//the -1 is b/c of how we shifted the loop in init
+							
+						//System.out.println(list[j]+" is not prime");	
+						list[j] = 0;
 					}
 	
 			}
 			lastScanNum = scanNum;
-			scanNum = findNextZero(newList);
+			scanNum = findNextNonZero(list);
 		}
 		
-		for (int num: newList){ 									//collect the numbers as a string to be printed
+		for (int num: list){ 										//collect the numbers as a string to be printed
 				if (num != 0)
 					primeLS+=" "+num;
 		}
@@ -50,8 +51,6 @@ public class Primes{												//tab width 4
 	public static void goldbach(int[] list){						//Sigh. b/c Sieve cannot return a value (as per instruction)
 																	//I have to copy the method from above, or create another that they can share...	
 		
-int[] newList = list;		
-
 		String toPrint = "";	
 		int[]  primeList = list;
 		int start = 0, scanNum, lastScanNum;		
@@ -60,38 +59,46 @@ int[] newList = list;
 		primeCNT = 0; 												//reset global variable 
 		list[0] = 0;										 		//1 is not a prime number
 																	//and if it were, counting it would set everything to 0
-		scanNum = findNextZero(newList);	
+		scanNum = findNextNonZero(list);	
 		lastScanNum = -1;
 		
 		while(scanNum != -1){										//the findNextZero method throws -1 when no new primes are found
 	
 			for (int j = (scanNum*2)-1; j<list.length;j+=scanNum){	//start zeroing from the second multiple of the num for this loop 
 				
-					if( primeList[j] != 0){							//the -1 is b/c of how we shifted the loop in init
-						primeList[j] = 0;	
+					if( list[j] != 0){								//the -1 is b/c of how we shifted the loop in init
+						primeCNT++;
+						list[j] = 0;
 					}
 	
 			}
 			lastScanNum = scanNum;
-			scanNum = findNextZero(newList);
+			scanNum = findNextNonZero(list);
 		}	
 
+		primeList = new int[primeCNT];
+		primeCNT = 0; 												//whenever I intend to loop findNextNonZero I need to first 0 primeCNT
 
-		for (int i = 4; i < list.length; i+=2){						//Start at 4
+		for (int i = 0; i<primeList.length; i++){
+			primeList[i] = findNextNonZero(list);
+		}
+																	//ok, over here we should have a MUCH smaller list to step through
+
+		for (int i = 4; i < list.length; i+=2){
 			pass = false;
 			for(int firstPrime:primeList){
 				if (isPrime(i - firstPrime,primeList) && !pass) {
 					System.out.println(firstPrime+" + "+(i - firstPrime)+" = "+i);
-					pass = true;
+					break;
 				}
-			}
+			}	 							
 
 		}
 
-		System.out.println(toPrint);	
+				System.out.println("Done");
 	}
 	
-	public static int findNextZero(int[] list){
+	public static int findNextNonZero(int[] list){
 		int cnt = 0; 												//#of non-zeros we have seen each loop
 		for (int i = 0; i<list.length; i++){						//step through list
 			if (list[i] !=0){ 										
@@ -102,7 +109,7 @@ int[] newList = list;
 				}
 			}
 		}
-		return -1;													//This really shouldn't happen...
+		return -1;													//no new primes found
 	}
 
 	public static boolean isPrime(int myNum,int[] primes){
